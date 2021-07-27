1c4ea3d961859a996fd91962940d3e533cd9f644
limzidi <65108469+limzidi@users.noreply.github.com>
2021-07-27 16:26:42 +0800

    p

---
+++ javascript.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function () {
+  $('#pills-QUALIFICATIONS-tab').click(function() {
+  $('html, body').animate({
+    scrollTop: $(".achievements").offset().top
+  }, 500)
+})
+})
+
+$(document).ready(function () {
+  $('.qualif').click(function() {
+  $('html, body').animate({
+    scrollTop: $(".achievements").offset().top
+  }, 500)
+})
+})
+
+
+
+$(document).ready(function () {
+  $('.researcher').click(function() {
+  $('html, body').animate({
+    scrollTop: $("#accordion").offset().top
+  }, 500)
+})
+})
+
+$(document).ready(function () {
+  $('.intere').click(function() {
+  $('html, body').animate({
+    scrollTop: $(".interestbox").offset().top
+  }, 500)
+})
+})
+
+$(document).ready(function () {
+  $('.contacts ').click(function() {
+  $('html, body').animate({
+    scrollTop: $(".achievements").offset().top
+  }, 500)
+})
+})
+
+$(document).ready(function () {
+  $('.qualif ').click(function() {
+  $('html, body').animate({
+    scrollTop: $(".achievements").offset().top
+  }, 500)
+})
+})
+
+
+alert ("under construction -lzd")
